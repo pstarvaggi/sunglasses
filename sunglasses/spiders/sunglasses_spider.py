9dd3f48f40377710eb59064b00d1a51693040806
kolmogorov7 <pstarvaggi@gmail.com>
2020-09-29 11:30:00 -0400

    loads all sunglasses in one page

--- sunglasses/spiders/sunglasses_spider.py
+++ sunglasses/spiders/sunglasses_spider.py
@@ -11,8 +11,17 @@ class SunglassesSpider(Spider):
     def parse(self, response):
         text = response.xpath('//*[@id="content"]/nav[1]/div[2]/span[1]/text()').extract_first()
         num_glasses = int(re.search('\d+',re.search('of\s\d+', text).group()).group())
-        num_pages = math.ceil(float(num_glasses)/24)
-        print('+'*77)
-        print(num_pages)
-        print('+'*77)
 
+        url = response.xpath('//*[@id="content"]/div[1]/div[2]/ul/li[3]/a/@href').extract_first()
+        url = re.sub('skip%3D24','skip%3D0', url)
+        url = re.sub('top%3D24','top%3D'+str(num_glasses), url)
+        url = re.sub('page_2','page_1', url)
+        url = "https://www.woot.com" + url
+
+        yield Request(url = url, callback = self.parse_glasses_list_page)
+
+    def parse_glasses_list_page(self, response):
+        glasses = response.xpath('//*[@id="content"]/div[1]/ul/li')
+        for pair in glasses:
+            print(pair.xpath('./a/span[@class="title"]/text()').extract_first())
+            print('+'*77)
